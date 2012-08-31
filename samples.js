974311c251dcec0a07e495b62bef5c55c1e693e7
Ryan Ryan <ryan.anklam@gmail.com>
2012-08-31 07:37:19 -0500

    more edits

--- samples.js
+++ samples.js
@@ -1,21 +1,36 @@
 var presentation = {
 	scoping : function(){
 		function doSomething(){
-		    myVar = "foo"; //this goes into the global scope which == bad
-		    var myOtherVar = "bar"; //this goes into the function scope which == good     
+			myVar = "foo"; //this goes into the global scope which == bad
+			var myOtherVar = "bar"; //this goes into the function scope which == good
 		}
 
 		doSomething();
 		
-		console.log(myVar); //foo 
+		console.log(myVar); //foo
 		console.log(myOtherVar); //error
+	},
+
+	andor3 : function(arg){
+
+		var myFunc = function(arg){
+
+			var myVal = document.getElementById(arg) ||
+			document.createElement("div"); //if no arg was passed in myVal is "defaultValue"
+
+			console.log(myVal);
+		};
+
+		myFunc();
+		myFunc("andor3");
 	}
 };
 
 $(function(){
 
 	$(document).on('click','input[type="button"]',function(){
-		presentation[$(this).data('run')]();
+		var $this = $(this);
+		presentation[$this.data('run')]($this.data('args'));
 	});
 
 });
