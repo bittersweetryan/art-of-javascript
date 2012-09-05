0479a0c17679a4abf4fadf3ba236365e6a64f5d5
Ryan Ryan <ryan.anklam@gmail.com>
2012-09-05 13:28:44 -0500

    create gh-pages

---
+++ README.md
@@ -0,0 +1,4 @@
+JavaScript is one of the fastest growing and most popular programming languages right now.  In todays web and mobile centric development landscape JavaScript is a powerful tool for any developer to know.  With approximately 20% of all github repositories and thousands of questions asked on stackoverflow daily its clear that JavaScript is here to stay.  
+
+Believe it or not ColdFusion and JavaScript have a lot of similarities between them. This meetup presentation will focus on teaching the fundamentals of JavaScript through the eyes of a ColdFusion developer.  Whether you are just getting started with JavaScript or you've only spent time writing code for your favorite JavaScript framework this session will help you learn the fundamentals and important concepts of the JavaScript programming language. 
+
---
+++ samples.js
@@ -0,0 +1,154 @@
+$(function(){
+	var presentation = {
+		scoping : function(){
+			function doSomething(){
+				myVar = "foo"; //this goes into the global scope which == bad
+				var myOtherVar = "bar"; //this goes into the function scope which == good
+			}
+
+			doSomething();
+			
+			console.log(myVar); //foo
+			console.log(myOtherVar); //error
+		},
+
+		andor3 : function(arg){
+
+			var myFunc = function(arg){
+
+				var myVal = document.getElementById(arg) ||
+				document.createElement("div"); //if no arg was passed in myVal is "defaultValue"
+
+				console.log(myVal);
+			};
+
+			myFunc();
+			myFunc("andor3");
+		},
+
+		args : function(){
+			var myFunc = function(){
+				console.log(arguments);
+			};
+
+			myFunc("Hello", "World", function(){});
+		},
+
+		closure : (function(){
+			var total = 0;
+
+			return function(){
+				total++;
+				console.log(total);
+			};
+		}()),
+
+		this1 : function(){
+			console.log( this );
+			window.myFunc();
+		},
+
+		this2 : function(){
+			var person = new Person("Bart");
+		},
+
+		this3 : function(){
+			myObj = {
+				name : "Homer",
+
+				sayHello : function(){
+					//look what happens when this is used inside a nested function
+					return (function(){
+						console.log("Hello, " + this.name);
+						console.log(this);
+					}());
+				}
+			};
+
+			myObj.sayHello();
+		},
+
+		inheritance1 : function(){
+			var Car = function(){ };
+
+			//add properties to Car's Prototype
+			Car.prototype = {
+				wheels : 4, doors : 4,  manufacturer : ""
+			};
+
+			var R8 = function(){ };
+
+			//set the R8's prototype to a new instance of car
+			R8.prototype = new Car();
+
+			R8.prototype.constructor = R8; //reset the R8's constructor
+
+			var r8 = new R8();
+
+			console.log(r8);
+			console.log(r8.wheels);
+		},
+
+		optionsPattern : function(){
+			$(document).myPlugin({color:'black', height: '500px'});
+			$(document).myPlugin();
+		}
+	};
+
+	$(document).on('click','input[type="button"]',function(){
+		var $this = $(this);
+
+		if(presentation[$this.data('run')]){
+			presentation[$this.data('run')]($this.data('args'));
+		}
+	});
+});
+
+
+//intentional globals
+var myFunc = function(){
+    console.log(this);
+};
+
+var obj = {
+    init : function(){
+        $('#thisButton').on('click',function(){
+            console.log(this);
+        });
+    }
+};
+
+obj.init();
+
+var obj1 = {
+    init : function(){
+        var self = this; //create a ref to the current "this" scope
+
+        $("#thisButton1").on('click',function(){
+            console.log(self);
+        });
+    }
+};
+
+obj1.init();
+
+function Person(name){
+    this.name = name;
+    
+    console.log(this);
+    console.log(this.name);
+}
+
+$.fn.myPlugin = function(options){
+    var defaults = {color: "blue", height: "200px" };
+
+    //remember to check the type of the argument!
+    if(typeof options === 'object'){
+        options = $.extend(defaults,options);
+    } else {
+        options = defaults;
+    }
+
+    console.log(options);
+};
+
